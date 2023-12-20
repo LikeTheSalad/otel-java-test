79f3fb5cce58837bf10f946ee1eb0ce67ebfafbf
Cesar Munoz <56847527+LikeTheSalad@users.noreply.github.com>
2023-12-20 17:27:37 +0100

    Adding json otlp exporter/logger for OTel logs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.logs.Logger;
+import io.opentelemetry.exporter.logging.otlp.OtlpJsonLoggingLogRecordExporter;
+import io.opentelemetry.exporter.logging.otlp.OtlpJsonLoggingSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.SimpleLogRecordProcessor;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        OpenTelemetry openTelemetry = setUpOpenTelemetry();
+
+        Logger logger = openTelemetry.getLogsBridge().loggerBuilder("LoggerScope").build();
+
+        logger.logRecordBuilder().setBody("Some body").emit();
+    }
+
+    private static OpenTelemetry setUpOpenTelemetry() {
+        return OpenTelemetrySdk.builder()
+                .setLoggerProvider(
+                        SdkLoggerProvider.builder()
+                                .addLogRecordProcessor(SimpleLogRecordProcessor.create(OtlpJsonLoggingLogRecordExporter.create()))
+                                .build()
+                ).setTracerProvider(
+                        SdkTracerProvider.builder()
+                                .addSpanProcessor(SimpleSpanProcessor.create(OtlpJsonLoggingSpanExporter.create()))
+                                .build())
+                .build();
     }
 }
